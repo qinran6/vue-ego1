a7d0ca7d67aff9a32276e642f2beb290db412260
qinran6 <3546960732@qq.com>
2025-04-04 14:16:03 +0800

    左侧导航导入

--- ego/src/views/Layout/Mymenu.vue
+++ ego/src/views/Layout/Mymenu.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <h2>导航</h2>
+    <ul>
+      <li>
+        <router-link to='/'>首页</router-link>
+      </li>
+      <li>
+        <router-link to='/goods'>商品管理</router-link>
+      </li>
+      <li>
+        <router-link to='/params'>规格参数</router-link>
+      </li>
+      <li>
+        <router-link to='/advert'>广告分类</router-link>
+      </li>
+      <li>
+        <router-link to='/order'>订单管理</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
