74f0870092e64e1be1abc5e46d7f1e4cdac00c08
qinran6 <3546960732@qq.com>
2025-04-05 23:27:53 +0800

    给table添加“编辑”和“删除”操作

--- ego/src/views/Goods/Goods.vue
+++ ego/src/views/Goods/Goods.vue
@@ -18,7 +18,19 @@
         <el-table-column prop="date" label="商品图片" ></el-table-column>
         <el-table-column prop="date" label="商品卖点" ></el-table-column>
         <el-table-column prop="date" label="商品描述" ></el-table-column>
-        <el-table-column prop="date" label="操作" width="180"></el-table-column>
+        <el-table-column prop="date" label="操作" width="180">
+          <!-- <template slot-scope="scope"> -->
+          <template #default="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+              
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
 
      </div>
@@ -30,10 +42,18 @@
 export default {
   data(){
     return{
-      input:''
-    }
+      input:'',
+      tableData:[{
+          date: '2016-05-02',
+        }],
+    };
+  },
+  methods:{
+    // 编辑操作
+    handleEdit(){},
+    // 删除操作
+    handleDelete(){}
   }
-
 }
 </script>
 
