9c961751f911fc88ad010ea020d0dc1cbbd7e6e9
qinran6 <3546960732@qq.com>
2025-04-09 22:54:47 +0800

    数据库debug

--- ego/server/router.js
+++ ego/server/router.js
@@ -1,54 +1,63 @@
-const express=require('express')
-const router=express.Router()
+const express = require('express')
+const router = express.Router()
+//导入数据库 sqlFn('sql',[],res=>{})
+const sqlFn = require('./mysql')
 
-//导入数据库 
-const sqlFn=require('./mysql');
-//const { Result } = require('element-ui');
-
-//获取分页
-router.get('/projectList',(req,res)=>{
-    const page=req.query.page||1;
-    const sqlLen="select*from project where id";
-    sqlFn(sqlLen,null,data=>{
-        let len=data.length;
-        const sql="select * from project order by id desc limit 8 offset"+(page-1)*8;
-        sqlFn(sql,null,result=>{
-            if(result.length>0){
+/**
+ * 商品列表：获取分页 {total:'',arr:[{},{},{}],pagesize:8,}
+ * 参数：page 页码
+ */
+router.get('/projectList', (req, res) => {
+    const page = req.query.page || 1;
+    const sqlLen = "select * from project where id";
+    sqlFn(sqlLen, null, data => {
+        let len = data.length;
+        const sql = "select * from project order by id desc limit 8 offset " + (page - 1) * 8;
+        sqlFn(sql, null, result => {
+            if (result.length > 0) {
                 res.send({
-                    status:200,
-                    data:result,
-                    pageSize:8,
-                    total:len
+                    status: 200,
+                    data: result,
+                    pageSize: 8,
+                    total: len
                 })
-            }else{
+            } else {
                 res.send({
-                    status:500,
-                    msg:"暂无数据"
+                    status: 500,
+                    msg: "暂无数据"
                 })
             }
         })
     })
 })
-//商品查询接口
-router.get("/search",(req,res)=>{
-    var search=req.query.search;
-    //待添加
-    const sql="select * from project where concat('title1','sellPoint','descs') like "%" + search + "%"";
-    sqlFn(sql,null,(result)=>{
-        if(result.length>0){
+
+/**
+ * 商品查询接口 search
+ * 参数：search
+ */
+router.get("/search", (req, res) => {
+    var search = req.query.search;
+    const sql = "select * from project where concat(`title`,`sellPoint`,`descs`) like '%" + search + "%'";
+    sqlFn(sql, null, (result) => {
+        if (result.length > 0) {
             res.send({
-                status:200,
+                status: 200,
                 result
             })
-        }else{
+        } else {
             res.send({
-                status:500,
-                msg:"暂无数据"
+                status: 500,
+                msg: "暂无数据"
             })
-        }               
+        }
     })
 })
 
 
 
-module.exports=router
+
+
+
+
+
+module.exports = router
--- ego/vue.config.js
+++ ego/vue.config.js
@@ -7,10 +7,10 @@ module.exports={
   devServer:{
     proxy:{
       '/api':{
-        target:'https://localhost:8989',
+        target:'http://localhost:8989/',
         ws: true,
         changeOrigin: true,
-        pathRewrite:{//重写路径
+        pathRewriter:{//重写路径
             '^/api':''
         }
       },
