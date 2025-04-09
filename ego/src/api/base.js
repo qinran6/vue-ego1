9a896814ada9d46a16912d95f130e94fc029ec34
qinran6 <3546960732@qq.com>
2025-04-09 21:05:09 +0800

    连接数据库

--- ego/src/api/base.js
+++ ego/src/api/base.js
@@ -1,7 +1,7 @@
 //接口路径配置
 
 const base={
-    goodList:'/api/projectList',//商品列表
+    goodsList:'/api/projectList',//商品列表
 }
 
 export default base;
--- ego/src/api/index.js
+++ ego/src/api/index.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 import base from './base'
 const api={
     //商品列表
-    getGoodsList(){//page:xx
-        return axios.get(base.goodList,{
+    getGoodsList(params){//page:xx
+        return axios.get(base.goodsList,{
             params
         })
     }
