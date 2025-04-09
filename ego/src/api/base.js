93461390b7573dff930ebddc96667cf4db881675
qinran6 <3546960732@qq.com>
2025-04-09 23:21:25 +0800

    debug

--- ego/src/api/base.js
+++ ego/src/api/base.js
@@ -1,7 +1,7 @@
 //接口路径配置
 
 const base={
-    goodsList:'/api/projectList',//商品列表
+    goodsList:'/api/api/projectList',//商品列表
 }
 
 export default base;
--- ego/vue.config.js
+++ ego/vue.config.js
@@ -1,19 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
 
-module.exports={
-  devServer:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:8989/',
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8989', // 去掉末尾斜杠（推荐）
         ws: true,
         changeOrigin: true,
-        pathRewriter:{//重写路径
-            '^/api':''
+        pathRewrite: { // 使用正确的选项名
+          '^/api': ''
         }
-      },
-    },
+      }
+    }
   }
 }
