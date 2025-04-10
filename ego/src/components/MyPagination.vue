73aa2692d42cedabb33bf1d1bc55f462bf190ee2
qinran6 <3546960732@qq.com>
2025-04-10 13:47:02 +0800

    引入分页组件

---
+++ ego/src/components/MyPagination.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-pagination background layout="prev, pager, next" :total="1000">
+  </el-pagination>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
