ad6bf72fed2ff454f19772f4797b3567a36176d8
qinran6 <3546960732@qq.com>
2025-04-10 13:59:05 +0800

    配置接收total和pageSize

--- ego/src/components/MyPagination.vue
+++ ego/src/components/MyPagination.vue
@@ -1,12 +1,21 @@
 <template>
-  <el-pagination background layout="prev, pager, next" :total="1000">
+  <el-pagination background layout="total,prev, pager, next,jumper" :total="total" :page-size="pageSize">
   </el-pagination>
 </template>
 
 <script>
 export default {
-
-}
+    props:{
+        total:{
+            type:Number,
+            default:100
+        },
+        pageSize:{
+            type:Number,
+            default:10
+        }
+    }
+};
 </script>
 
 <style>
