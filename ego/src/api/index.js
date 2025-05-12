aaee4eed1b0cde86c8c96bc43421c5d7dc6e4fa3
creddly <2332022195@qq.com>
2025-05-12 13:53:33 +0800

    规格参数获取

--- ego/src/api/index.js
+++ ego/src/api/index.js
@@ -1,56 +1,105 @@
-//请求的方法
+/**
+ * 请求的方法
+ */
 
 import axios from 'axios'
 import base from './base'
-//node.js
-const qs=require('querystring')
+//node>js 
+const qs = require('querystring')
 
-const api={
-    //登录接口
-    getLogin(params){//params={username:'',password:''}
-        console.log(base.login);
-        return axios.post(base.login,qs.stringify(params))
+const api = {
+    /**
+     * 登录接口
+     */
+    getLogin(params) {//params={username:'',password:''}
+        return axios.post(base.login, qs.stringify(params))
     },
-    //商品列表
-    getGoodsList(params){//page:xx
-        return axios.get(base.goodsList,{
+    /**
+     * 商品列表
+     */
+    getGoodsList(params) {//{page:xx}
+        return axios.get(base.goodsList, {
             params
         })
     },
-    //搜索商品数据
-    getSearch(params){//{search:xx}
-        return axios.get(base.search,{params})
+    /**
+     * 搜索商品数据
+     * search
+     */
+    getSearch(params) {//{search:xx}
+        return axios.get(base.search, { params })
     },
-
-    //获取类目选择
-    getSelectCategory(params){
-        return axios.get(base.selectCategory,{params})
+    /**
+     * 获取类目选择
+     * {id：cid}
+     */
+    getSelectCategory(params) {
+        return axios.get(base.selectCategory, { params })
     },
-    /* 
-        添加商品
-        title cid  category sellPoint price num descs paramsInfo image
+    /**
+     * 添加商品
+     * 参数： title cid  category sellPoint price num desc paramsInfo image
+     */
+    addGoods(params) {//={}
+        return axios.get(base.addGoods, {
+            params
+        })
+    },
+    /**
+     * 删除商品 id
+     */
+    deleteGoods(params) {
+        return axios.get(base.deleteGoods, { params })
+    },
+    /**
+     * 编辑商品id
+     */
+    updateGoods(params) {
+        return axios.get(base.updateGoods, { params })
+    },
+    /**
+    * 统计数据
     */
-   addGoods(params){//={}
-        return axios.get(base.addGoods,{
-            params
-        })
-   },
-   /* 
-    删除商品
-   */
-   deleteGoods(params){
-    return axios.get(base.deleteGoods,{params})
-   },
-   /* 
-   编辑商品id
-   */
-   updateGoods(params){
-    return axios.get(base.updateGoods,{params})
-   },
-   /*规格参数获取列表 page:xx*/
-   getParams(params){
-        return axios.get(base.params,{params})
-   }
+    getStatistical() {
+        return axios.get(base.statistical)
+    },
+    /**
+     * 销量对比
+     */
+    getSellTotal() {
+        return axios.get(base.sellTotal)
+    },
+    /**
+     * 规格参数获取列表 
+     * page：xx
+     */
+    getParams(params) {
+        return axios.get(base.params, { params })
+    },
+    /**
+     * 获取订单数据
+     * currPage:xx
+     */
+     orderList(params){
+         return axios.get(base.orderList,{params})
+     },
+     /**
+      * 规格参数新增
+      *  参数：itemCatId,content,specsName
+      */
+      insertItemParam(params){
+          return axios.get(base.insertItemParam,{params})
+      },
+      /**
+       * 商品列表---获取类目规格配置
+       * cid
+       */
+       categoryData(params){
+           return axios.get(base.categoryData,{params})
+       }
+
+
+
 }
 
 export default api
